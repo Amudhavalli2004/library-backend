a846ff4eacaad14727887dab8786cdebe358ddaf
Amudhavalli R <amudhameenaravi@gmail.com>
2024-02-21 23:33:20 +0530

    second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'amudha',
+  password: 'Amudha7!',
   database: 'library',
   port: '3306',
 })
